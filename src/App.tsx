b9fed9d5069d2c87cc97c57050ec8d8c1410733e
ab <ab@asdasd.csm>
2022-08-28 12:18:24 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: top;
+min-height: 100vh;
+`
+const Card = styled.div`
+position: absolute;
+top: ${props => props.position*30}px;
+width: 90vw;
+height: 70vh;
+background-color: ${props => props.color};
+border-radius: 5%;
+`
+
+const cards = [
+  {
+    title: 'Дикси',
+    color: 'orange',
+  },
+  {
+    title: 'Магнит',
+    color: 'red',
+  },
+]
+
+function App() {
+  return (
+    <Container>
+      {cards.map((card,ind) => {
+        return (
+          <Card key={card.title} color={card.color} position={ind}>{card.title}</Card>
+        )
+      })}
+    </Container>
+  );
+}
+
+export default App;
