c084c96f7e2d644c348ae8ab6f890f917a5874e0
ab <ab@sdflkj.csm>
2022-08-30 01:01:49 +0300

    add cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 import styled from 'styled-components'
+import metroJPG from './images/metro.jpg'
+import miratorgJPG from './images/miratorg.jpg'
+import vernyiJPG from './images/vernyi.jpg'
+import sparJPG from './images/spar.jpg'
+import diksiJPG from './images/diksi.jpg'
+import hmJPG from './images/hm.jpg'
+import okeyJPG from './images/okey.jpg'
+import lentaJPG from './images/lenta.jpg'
+import perekrestokJPG from './images/perekrestok.jpg'
+import pyaterkaJPG from './images/pyaterka.jpg'
 
 const Container = styled.div`
 display: flex;
@@ -30,44 +40,66 @@ width: 90%;
 const CardImage = styled.main`
 width: 100%;
 height: calc(100% - 55px + 10px);
-background-color: purple;
+background-color: ${props => props.color};
 border-radius: 0 0 15px 15px;
+background-image: url(${(props: { image: string }) => props.image});
+background-size: contain;
+background-repeat: no-repeat;
+background-position: center;
 `
 const Title = styled.div`
 height: 55px;
 `
+const lcard = {
+  title: 'Лента',
+  color: '#253c88',
+  barcode: lentaJPG,
+}
 const cards = [
   {
     title: 'Дикси',
     color: 'orange',
+    barcode: diksiJPG,
   },
   {
-    title: 'Магнит',
-    color: 'red',
+    title: 'Верный',
+    color: '#e01e27',
+    barcode: vernyiJPG,
+  },
+  {
+    title: 'Хмельная миля',
+    color: '#f0cc33',
+    barcode: hmJPG,
+  },
+  {
+    title: 'Metro',
+    color: '#1a3c7b',
+    barcode: metroJPG,
+  },
+  {
+    title: 'SPAR',
+    color: '#e01c22',
+    barcode: sparJPG,
+  },
+  {
+    title: 'OKEY',
+    color: 'gold',
+    barcode: okeyJPG,
+  },
+  {
+    title: 'Мираторг',
+    color: 'white',
+    barcode: miratorgJPG,
   },
   {
     title: 'Перекресток',
-    color: 'darkgreen',
+    color: '#005526',
+    barcode: perekrestokJPG,
   },
   {
-    title: 'Икея',
-    color: 'blue',
-  },
-  {
-    title: 'Утконос',
-    color: 'yellow',
-  },
-  {
-    title: 'Сбербанк',
-    color: 'green',
-  },
-  {
-    title: 'Четыре лапы',
-    color: 'orange',
-  },
-  {
-    title: 'Леруа Мерлен',
-    color: 'white',
+    title: 'Пятерочка',
+    color: '#e22b22',
+    barcode: pyaterkaJPG,
   },
 ]
 
@@ -77,6 +109,13 @@ function App() {
 
   function handleClick() {
     setCardOpen(!cardOpen)
+    if (cardOpen) {
+      (Array.from(document.getElementsByClassName('cardview') as HTMLCollectionOf<HTMLElement>))
+        .map(item => {
+          item.classList.add('card')
+          item.classList.remove('cardview')
+        })
+    }
   }
   function openView(e: React.MouseEvent<HTMLElement>) {
     if (e.currentTarget.classList.contains("cardview")) {
@@ -98,16 +137,16 @@ function App() {
             <Title>
               {card.title}
             </Title>
-            <CardImage />
+            <CardImage image={card.barcode} />
           </Card>
         )
       })}
 
-      <LastCard color={cards[0].color} className="card" onClick={e => { openView(e) }}>
+      <LastCard color={lcard.color} className="card" onClick={e => { openView(e) }}>
         <Title>
-          {cards[0].title}
+          {lcard.title}
         </Title>
-        <CardImage />
+        <CardImage image={lcard.barcode} />
       </LastCard>
     </Container>
   );
