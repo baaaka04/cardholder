12ec8723bbcd05875e637e615b06eae2dbce354f
ab <ab@sdflkj.csm>
2022-08-30 13:39:36 +0300

    new card, minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,8 @@
 import { useState } from 'react'
 import styled from 'styled-components'
-import metroJPG from './images/metro.jpg'
-import miratorgJPG from './images/miratorg.jpg'
-import vernyiJPG from './images/vernyi.jpg'
-import sparJPG from './images/spar.jpg'
-import diksiJPG from './images/diksi.jpg'
-import hmJPG from './images/hm.jpg'
-import okeyJPG from './images/okey.jpg'
-import lentaJPG from './images/lenta.jpg'
-import perekrestokJPG from './images/perekrestok.jpg'
-import pyaterkaJPG from './images/pyaterka.jpg'
-
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: top;
-min-height: 100vh;
-padding-bottom: 40px;
-`
-const Card = styled.div`
-width: 90%;
-top: 40px;
-position: sticky;
-height: 40px;
-background-color: ${props => props.color};
-box-shadow: 0 10px ${props => props.color};
-border-radius: 15px 15px 0 0;
-`
-const LastCard = styled(Card)`
-height: 60vh;
-border-radius: 15px;
-`
-const Top = styled.div`
-height: 40px;
-top: 40px;
-width: 90%;
-`
-const CardImage = styled.main`
-width: 100%;
-height: calc(100% - 55px + 10px);
-background-color: ${props => props.color};
-border-radius: 0 0 15px 15px;
-background-image: url(${(props: { image: string }) => props.image});
-background-size: contain;
-background-repeat: no-repeat;
-background-position: center;
-`
-const Title = styled.div`
-height: 55px;
-`
-const lcard = {
-  title: 'Лента',
-  color: '#253c88',
-  barcode: lentaJPG,
-}
-const cards = [
-  {
-    title: 'Дикси',
-    color: 'orange',
-    barcode: diksiJPG,
-  },
-  {
-    title: 'Верный',
-    color: '#e01e27',
-    barcode: vernyiJPG,
-  },
-  {
-    title: 'Хмельная миля',
-    color: '#f0cc33',
-    barcode: hmJPG,
-  },
-  {
-    title: 'Metro',
-    color: '#1a3c7b',
-    barcode: metroJPG,
-  },
-  {
-    title: 'SPAR',
-    color: '#e01c22',
-    barcode: sparJPG,
-  },
-  {
-    title: 'OKEY',
-    color: 'gold',
-    barcode: okeyJPG,
-  },
-  {
-    title: 'Мираторг',
-    color: 'white',
-    barcode: miratorgJPG,
-  },
-  {
-    title: 'Перекресток',
-    color: '#005526',
-    barcode: perekrestokJPG,
-  },
-  {
-    title: 'Пятерочка',
-    color: '#e22b22',
-    barcode: pyaterkaJPG,
-  },
-]
+import { lcard, cards } from './Cards'
 
 function App() {
-
   const [cardOpen, setCardOpen] = useState<boolean>(false)
 
   function handleClick() {
@@ -127,14 +25,13 @@ function App() {
     }
   }
 
-
   return (
     <Container className={cardOpen ? "overview" : ""} onClick={handleClick}>
       <Top />
       {cards.map(card => {
         return (
           <Card key={card.title} color={card.color} className="card" onClick={e => { openView(e) }}>
-            <Title>
+            <Title fontColor={card.font}>
               {card.title}
             </Title>
             <CardImage image={card.barcode} />
@@ -143,7 +40,7 @@ function App() {
       })}
 
       <LastCard color={lcard.color} className="card" onClick={e => { openView(e) }}>
-        <Title>
+        <Title fontColor={lcard.font}>
           {lcard.title}
         </Title>
         <CardImage image={lcard.barcode} />
@@ -151,5 +48,55 @@ function App() {
     </Container>
   );
 }
+
+document.documentElement.classList.add('dark') // define background color
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: top;
+min-height: 100vh;
+padding-bottom: 40px;
+`
+const Card = styled.div`
+width: 90%;
+max-width: 380px;
+top: 40px;
+position: sticky;
+height: 40px;
+background-color: ${props => props.color};
+box-shadow: 0 10px ${props => props.color};
+border: 1px solid gray;
+border-bottom: 0;
+border-radius: 15px 15px 0 0;
+`
+const LastCard = styled(Card)`
+height: 60vh;
+max-height: 520px;
+border-radius: 15px;
+`
+const Top = styled.div`
+height: 40px;
+top: 40px;
+width: 90%;
+`
+const CardImage = styled.main`
+width: 100%;
+height: calc(100% - 65px + 10px);
+background-color: ${props => props.color};
+border-radius: 0 0 15px 15px;
+background-image: url(${(props: { image: string }) => props.image});
+background-size: contain;
+background-repeat: no-repeat;
+background-position: center;
+`
+const Title = styled.div`
+color: ${(props: { fontColor: string }) => props.fontColor};
+font-size: 18px;
+font-weight: 500;
+padding-top: 10px;
+height: 55px;
+`
 
 export default App;
---
+++ src/Cards.tsx
@@ -0,0 +1,81 @@
+import metroJPG from './images/metro.jpg'
+import miratorgJPG from './images/miratorg.jpg'
+import vernyiJPG from './images/vernyi.jpg'
+import sparJPG from './images/spar.jpg'
+import diksiJPG from './images/diksi.jpg'
+import hmJPG from './images/hm.jpg'
+import okeyJPG from './images/okey.jpg'
+import lentaJPG from './images/lenta.jpg'
+import perekrestokJPG from './images/perekrestok.jpg'
+import pyaterkaJPG from './images/pyaterka.jpg'
+import vinlabJPG from './images/vinlab.jpg'
+
+
+export const lcard = {
+    title: 'Лента',
+    color: '#253c88',
+    barcode: lentaJPG,
+    font: '#f1d613',
+}
+export const cards = [
+    {
+        title: 'Дикси',
+        color: 'orange',
+        barcode: diksiJPG,
+        font: 'white',
+    },
+    {
+        title: 'Верный',
+        color: '#e01e27',
+        barcode: vernyiJPG,
+        font: 'white',
+    },
+    {
+        title: 'Хмельная миля',
+        color: '#f0cc33',
+        barcode: hmJPG,
+        font: '#3e0000',
+    },
+    {
+        title: 'Metro',
+        color: '#1a3c7b',
+        barcode: metroJPG,
+        font: '#f1d500',
+    },
+    {
+        title: 'SPAR',
+        color: '#e01c22',
+        barcode: sparJPG,
+        font: 'white',
+    },
+    {
+        title: "O'KEY",
+        color: 'gold',
+        barcode: okeyJPG,
+        font: '#d70612',
+    },
+    {
+        title: 'Мираторг',
+        color: '#77bb02',
+        barcode: miratorgJPG,
+        font: 'white',
+    },
+    {
+        title: 'Перекресток',
+        color: '#005526',
+        barcode: perekrestokJPG,
+        font: 'white',
+    },
+    {
+        title: 'Пятерочка',
+        color: '#e22b22',
+        barcode: pyaterkaJPG,
+        font: 'white',
+    },
+    {
+        title: 'Winelab | ВИНЛАБ',
+        color: '#282b33',
+        barcode: vinlabJPG,
+        font: 'white',
+    },
+]
